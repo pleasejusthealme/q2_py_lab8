bb6f46ba9656043af394d2ce618f26c3ca3b0769
sam! <120393338+pleasejusthealme@users.noreply.github.com>
2026-05-22 14:02:16 +0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -29,3 +29,27 @@ button.pack()
 
 window.mainloop()
 
+import random
+
+# генерация списка случайных чисел
+numbers = [random.randint(1, 100) for _ in range(10)]
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
+
+print("Исходный список:", numbers)
+
+sorted_numbers = insertion_sort(numbers)
+
+print("Отсортированный список:", sorted_numbers)
+
